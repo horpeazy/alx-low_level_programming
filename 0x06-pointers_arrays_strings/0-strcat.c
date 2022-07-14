dacfa81248b3d7e62592a95c986879da065ee3e3
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-14 02:59:30 -0700

    feat - concatenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - takes two strings and concatenates them together
+ *
+ * return - A pointer to the concatenated strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    int i, j;
+
+    for(i = 0; dest[i] != '\0'; i++)
+	;
+    for(j = 0; src[j] != '\0'; j++, i++){
+	dest[i] = src[j];
+    }
+    dest[i] = '\0';
+
+    return(dest);
+}
+
+
