481f8d9dfe92012ad1637cc25788f8c2921bbe8a
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-18 20:23:07 -0700

    feat Added all the files for the task

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * _memset - Fills the memory with a constant byte
- * @s array to fill
- * @b value to fill in array
- * @n number of bytes to fill
- * return - returns a pointer to s
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ *
+ * Return: pointer to the memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-    int i = 0;
+	unsigned int i;
 
-    while(s[i] != '\0' && i < n){
-	s[i] = b;
-	i++;
-    }
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-    return(s);
+	return (s);
 }
