c5d740f402f00bda9374cc32dc2dd62167a06c39
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-21 15:18:35 -0700

    feat refactored

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  * _puts_recursion - prints a string without using a loop
@@ -11,10 +10,10 @@
 void _puts_recursion(char *s)
 {
     if (*s == '\0') {
-        write(1, '\n', 1);
+        _putchar('\n');
 	return;
     }
-    write(1, *s, 1);
+    _putchar(*s);
     _puts_recursion(s + 1);
 
 }
