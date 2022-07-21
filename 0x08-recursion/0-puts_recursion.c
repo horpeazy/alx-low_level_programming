d4e49a1a334ad8f00542ef9ca8fe8c9fa4ed2ce8
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-21 15:00:42 -0700

    feat removed return statement and updated pointer compariosn

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,12 +9,10 @@
 
 void _puts_recursion(char *s)
 {
-    if (s == '\0') {
+    if (*s == '\0') {
         _putchar('\n');
-        return(0);
     }
     _putchar(s);
     _puts_recursion(s + 1);
 
-    return(0);
 }
