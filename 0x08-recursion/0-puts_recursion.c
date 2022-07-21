e3de32a5f56cc0bd3c0318dc6f4aa168d7228f9c
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-21 15:29:51 -0700

    updated for beety style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,15 @@
 
 /**
  * _puts_recursion - prints a string without using a loop
- * @s - the string to print
+ * @s: - the string to print
  *
  * return - always return 0
  */
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') {
+    if(*s == '\0')
+    {
         _putchar('\n');
 	return;
     }
