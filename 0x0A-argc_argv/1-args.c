b7db87944bb50a90441395775dcfd518e4d1eeea
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-22 17:15:06 -0700

    feat - args file to print number of argumenst

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argumenst passed to it
+ * @argc: argument counter
+ *
+ * Return: always return 0
+ */
+
+int main(int argc)
+{
+	printf("%d\n", argc - 1);
+}
