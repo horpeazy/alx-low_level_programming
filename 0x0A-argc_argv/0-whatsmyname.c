13f2ebc0d28132c4263e71482d7342cff2a8bde9
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-22 17:01:59 -0700

    feat - program to print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: argument counter
+ * @argv: arguments vector
+ *
+ * Return: - Always return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
