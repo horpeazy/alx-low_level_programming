b1184f8cfe088d80ecad92f3d5064d9e05eb8782
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-22 17:42:43 -0700

    feat - corrected for betty

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,6 +3,7 @@
 /**
  * main - prints the number of argumenst passed to it
  * @argc: argument counter
+ * @argv: array taht contains progarms arguments
  *
  * Return: always return 0
  */
@@ -10,5 +11,5 @@
 int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-        return (0);
+	return (0);
 }
