8b26214e3932d7d98c24819fe6d44dc7603b88d4
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-22 18:55:46 -0700

    feat - adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - prints the result of adding all the input arguments
+ * @argc: argumenst counter
+ * @argv: array thta contains all the arguments
+ *
+ * Return: 0 - success , 1 - inavlid number
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int result;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return;
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[i]);
+	}
+
+	printf("%d\n", result);
+	return (0);
+}
