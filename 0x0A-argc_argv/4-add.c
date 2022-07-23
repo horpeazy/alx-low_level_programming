7348693d6d26c485a2774f61287d519f24f64e66
horpeazy <iyamuhope.nosa647@gmail.com>
2022-07-22 19:00:12 -0700

    feat refactored for optimization

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,13 +13,8 @@
 int main(int argc, char *argv[])
 {
 	int i, j;
-	int result;
+	int result = 0;
 
-	if (argc < 2)
-	{
-		printf("0\n");
-		return;
-	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
